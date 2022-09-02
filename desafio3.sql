d9f0ab16d31cc101819855b2e2e67cadc314a4de
Rodrigo Macedo <rodrigomac3do@gmail.com>
2022-09-02 11:18:20 -0300

    desafio3: QUERY retorna minutos e vezes que um usuario ouviu musica

---
+++ desafio3.sql
@@ -0,0 +1,12 @@
+SELECT
+    u.`name` AS usuario,
+    COUNT(h.id_user) AS qt_de_musicas_ouvidas,
+    ROUND(SUM(s.duration_time) / 60, 2) AS total_minutos
+FROM
+    SpotifyClone.`history` AS h
+    INNER JOIN SpotifyClone.`user` AS u ON h.id_user = u.id_user
+    INNER JOIN SpotifyClone.`song` AS s ON h.id_song = s.id_song
+GROUP BY
+    h.id_user
+ORDER BY
+    u.`name` ASC;
