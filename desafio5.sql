37c657e946cb1e856ea8629a143186474938c5d4
Rodrigo Macedo <rodrigomac3do@gmail.com>
2022-09-02 17:12:15 -0300

    desafio5: QUERY traz musicas mais tocadas em ordem alfabetica

---
+++ desafio5.sql
@@ -0,0 +1,19 @@
+SELECT
+  cancao,
+  COUNT(h.id_song) AS reproducoes
+FROM
+  SpotifyClone.`history` AS h
+  INNER JOIN(
+    SELECT
+      id_song,
+      `name` AS cancao
+    FROM
+      SpotifyClone.`song`
+  ) AS s ON h.id_song = s.id_song
+GROUP BY
+  h.id_song
+ORDER BY
+  reproducoes DESC,
+  cancao ASC
+LIMIT
+  2;
