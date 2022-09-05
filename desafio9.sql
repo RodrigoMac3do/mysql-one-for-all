d2c1af16e08d779be103c2c85394976f853520f2
Rodrigo Macedo <rodrigomac3do@gmail.com>
2022-09-04 22:08:14 -0300

    style: formatação

--- desafio9.sql
+++ desafio9.sql
@@ -2,6 +2,6 @@ SELECT
   COUNT(*) AS quantidade_musicas_no_historico
 FROM
   SpotifyClone.`history` AS h
-  INNER JOIN SpotifyClone.user AS u ON u.id_user = h.id_user
+  INNER JOIN SpotifyClone.`user` AS u ON u.id_user = h.id_user
 WHERE
   `name` = 'Barbara Liskov';
